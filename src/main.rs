a0e354ce5e134a8db2556913c24f752c90e8714a
lumo03 <luis.moncada.mso@gmail.com>
2023-01-19 21:17:56 +0100

    improve code and add error handling

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,42 @@
-use std::env::{args, Args};
+use std::env::args;
 
 fn main() {
-    let mut args: Args = args();
+    let args: Vec<String> = args().skip(1).collect();
+    if args.len() != 3 {
+        panic!("Error: Invalid number of arguments.\nPlease provide three arguments: <first number> <operator> <second number>");
+    }
 
-    let first = args.nth(1).unwrap();
-    let operator = args.nth(0).unwrap().chars().next().unwrap();
-    let second = args.nth(0).unwrap(); 
+    let first_number = match args[0].parse::<f32>() {
+        Ok(n) => n,
+        Err(_) => {
+            panic!("Error: Invalid first number.");
+        }
+    };
 
-    let first_number = first.parse::<f32>().unwrap();
-    let second_number = second.parse::<f32>().unwrap();
+    let operator = match args[1].chars().next() {
+        Some(c) if c == '+' || c == '-' || c == '*' || c == 'x' || c == 'X' || c == '/' => c,
+        _ => {
+            panic!("Error: Invalid operator. Please use one of the following: +, -, *, x, X, /");
+        }
+    };
+
+    let second_number = match args[2].parse::<f32>() {
+        Ok(n) => n,
+        Err(_) => {
+            panic!("Error: Invalid second number.");
+        }
+    };
+
     let result = operate(operator, first_number, second_number);
-
-    println!("{:?}", output(first_number, operator, second_number, result));
+    println!("{} {} {} = {}", first_number, operator, second_number, result);
 }
 
 fn operate(operator: char, first_number: f32, second_number: f32) -> f32 {
     match operator {
         '+' => first_number + second_number,
         '-' => first_number - second_number,
-        '/' => first_number / second_number,
         '*' | 'x' | 'X' => first_number * second_number,
+        '/' => first_number / second_number,
         _ => panic!("Invalid operator used.")
     }
-}
-
-fn output(first_number: f32, operator: char, second_number: f32, result: f32) -> String {
-    format!("{} {} {} = {}", first_number, operator, second_number, result)
 }
